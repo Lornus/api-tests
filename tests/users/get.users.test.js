9fc00840a02b68466fe34e15521303a031aa94b2
Lornus <petrenko.fpm@gmail.com>
2021-08-27 13:22:42 +0300

    first test

---
+++ tests/users/get.users.test.js
@@ -0,0 +1,11 @@
+const got = require('got')
+const assert = require('assert')
+
+describe('User can', function (){
+        it('get user by id',  async function (){
+        const response = got('https://npplanets.herokuapp.com/users/6036a50333d65c355e25f050')
+        //const body = JSON.parse(response.body)
+        // assert(body.id == 6036a50333d65c355e25f050, `Expectet body to have ID ${body.id}` )
+        assert(response === true)
+    })
+})
